ce8bd9e5d6057337bd617d34fc4fb83b09b0968b
christianbollacker <christianbollacker@hotmail.com>
2015-03-01 23:14:14 -0800

    Add sort order choice and reverse choice

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,45 @@
 $(function() {
   $('#tenNumbers').hide();
-  $('#sort').hide();
+  $('#sortup').hide();
+  $('#sortdown').hide();
+  $('#sort').hide(); 
 
   $('#submit').on('click', function() {
     userBlocks = parseInt ($('#userChoice').val());
     $('#userNumber').fadeOut('fast');
     $('#tenNumbers').slideDown('slow');
-  });
+  });  
 
   $('#tenNumbers').on('click', function() {
     $('#tenNumbers').fadeOut('fast');
-    $('#sort').slideDown('slow');
+    $('#sortup').show('slow');
+    $('#sortdown').show('slow'); 
     generator(userBlocks);
+  });
+
+  $('#sortup').on('click', function() {
+    ascending = false;
+    sort();
+    $('#sortup').hide();
+    $('#sortdown').hide();
+    $('#sort').show();
+  });
+
+  $('#sortdown').on('click', function() {
+    ascending = true;
+    sort();
+    $('#sortup').hide();
+    $('#sortdown').hide();
+    $('#sort').show();
   });
 
   $('#sort').on('click', function() {
     sort();
   });
 
-
-
   var userBlocks = 10;
+
+  var ascending = true;
 
   function Item() {
     this.value = (Math.floor(Math.random() * 100));
@@ -33,15 +52,18 @@ $(function() {
     for (var i = num; i > 0; i--) {
       var item = new Item();
       $('.sortTable').append(item.toHTML());
-    }
+    }  
   }
 
   function sort() {
     var toSort = $('.sortTable div');
+
+    ascending = !ascending
+
     toSort.sort(function (a, b) {
       a = parseInt($(a).text());
       b = parseInt($(b).text());
-      return b - a;
+      return (ascending) ? a - b : b - a;
    });
    $('.sortTable').html(toSort);
   }
